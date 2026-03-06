a54de4ccc7eb232ec036ce5ae6791872fc9cbaa4
ritikamadaan18 <rashishvi@gmail.com>
2026-03-06 14:25:47 -0600

    Added rate limiting using arcjet

---
+++ backend/src/middleware/arcjet.middleware.js
@@ -0,0 +1,38 @@
+import aj from "../lib/arcjet.js";
+import { isSpoofedBot } from "@arcjet/inspect";
+import { ENV } from "../lib/env.js";
+
+export const arcjetProtection = async (req, res, next) => {
+  try {
+    const decision = await aj.protect(req);
+    console.log("here1", decision.isDenied());
+    console.log("result", JSON.stringify(decision, null, 2));
+    console.log("env", ENV.ARCJET_KEY);
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        return res
+          .status(429)
+          .json({ message: "Rate limit exceeded. Please try again later." });
+      } else if (decision.reason.isBot()) {
+        return res.status(403).json({ message: "Bot access denied." });
+      } else {
+        return res.status(403).json({
+          message: "Access denied by security policy.",
+        });
+      }
+    }
+
+    // check for spoofed bots
+    if (decision.results.some(isSpoofedBot)) {
+      return res.status(403).json({
+        error: "Spoofed bot detected",
+        message: "Malicious bot activity detected.",
+      });
+    }
+
+    next();
+  } catch (error) {
+    console.log("Arcjet Protection Error:", error);
+    next();
+  }
+};
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -6,9 +6,15 @@ import {
   updateProfile,
 } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 const router = Router();
 
+router.use(arcjetProtection);
+
 router.post("/signup", signup);
+router.get("/test", (req, res) => {
+  res.status(200).json({ message: "Test endpoint is working!" });
+});
 
 router.post("/login", login);
 
