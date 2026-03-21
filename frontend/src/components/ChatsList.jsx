d3b8503e4f4553fb946aa9b50067fa3d38e5573b
ritikamadaan18 <rashishvi@gmail.com>
2026-03-21 01:12:50 -0500

    Added online users functionality

--- frontend/src/components/ChatsList.jsx
+++ frontend/src/components/ChatsList.jsx
@@ -16,7 +16,33 @@ const ChatsList = () => {
 
   if (isUsersLoading) return <UsersLoadingSkeleton />;
   if (chats.length === 0) return <NoChatsFound />;
-  return <div></div>;
+  return (
+    <>
+      {chats.map((chat) => (
+        <div
+          key={chat._id}
+          className="bg-cyan-500/10 p-4 rounded-lg cursor-pointer hover:bg-cyan-500/20 transition-colors"
+          onClick={() => setSelectedUser(chat)}
+        >
+          <div className="flex items-center gap-3">
+            <div
+              className={`avatar ${onlineUsers.includes(chat._id) ? "online" : "offline"}`}
+            >
+              <div className="size-12 rounded-full">
+                <img
+                  src={chat.profilePic || "/avatar.png"}
+                  alt={chat.fullName}
+                />
+              </div>
+            </div>
+            <h4 className="text-slate-200 font-medium truncate">
+              {chat.fullName}
+            </h4>
+          </div>
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default ChatsList;
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,16 +1,24 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
+import { io } from "socket.io-client";
 
-export const useAuthStore = create((set) => ({
+const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:3000" : "/";
+
+export const useAuthStore = create((set, get) => ({
   authUser: null,
   isCheckingAuth: true,
   isSigningUp: false,
+  isLoggingIn: false,
+  socket: null,
+  onlineUsers: [],
 
   checkAuth: async () => {
     try {
       const res = await axiosInstance.get("/auth/check");
       set({ authUser: res.data });
+      get().connectSocket();
     } catch (error) {
       console.log("Error in authCheck:", error);
       set({ authUser: null });
@@ -26,6 +34,7 @@ export const useAuthStore = create((set) => ({
       set({ authUser: res.data });
 
       toast.success("Account created successfully!");
+      get().connectSocket();
     } catch (error) {
       toast.error(
         error.response?.data?.message || "Signup failed. Please try again.",
@@ -43,7 +52,7 @@ export const useAuthStore = create((set) => ({
 
       toast.success("Logged in successfully");
 
-      // get().connectSocket();
+      get().connectSocket();
     } catch (error) {
       toast.error(error.response.data.message);
     } finally {
@@ -56,7 +65,7 @@ export const useAuthStore = create((set) => ({
       await axiosInstance.post("/auth/logout");
       set({ authUser: null });
       toast.success("Logged out successfully");
-      // get().disconnectSocket();
+      get().disconnectSocket();
     } catch (error) {
       toast.error("Error logging out");
       console.log("Logout error:", error);
@@ -75,5 +84,27 @@ export const useAuthStore = create((set) => ({
           "Failed to update profile. Please try again.",
       );
     }
+  },
+
+  connectSocket: () => {
+    const { authUser } = get();
+    if (!authUser || get().socket?.connected) return;
+
+    const socket = io(BASE_URL, {
+      withCredentials: true, // this ensures cookies are sent with the connection
+    });
+
+    socket.connect();
+
+    set({ socket });
+
+    // listen for online users event
+    socket.on("getOnlineUsers", (userIds) => {
+      set({ onlineUsers: userIds });
+    });
+  },
+
+  disconnectSocket: () => {
+    if (get().socket?.connected) get().socket.disconnect();
   },
 }));
