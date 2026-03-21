c5b232b040764b372bd58f813396987f9e29eab8
ritikamadaan18 <rashishvi@gmail.com>
2026-03-21 01:20:51 -0500

    Added multi-tab/multi-device support per user

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -23,22 +23,52 @@ export function getReceiverSocketId(userId) {
 }
 
 // this is for storig online users
-const userSocketMap = {}; // {userId:socketId}
+// const userSocketMap = {}; // {userId:socketId}
+
+// io.on("connection", (socket) => {
+//   console.log("A user connected", socket.user.fullName);
+
+//   const userId = socket.userId;
+//   userSocketMap[userId] = socket.id;
+
+//   // io.emit() is used to send events to all connected clients
+//   io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+//   // with socket.on we listen for events from clients
+//   socket.on("disconnect", () => {
+//     console.log("A user disconnected", socket.user.fullName);
+//     delete userSocketMap[userId];
+//     io.emit("getOnlineUsers", Object.keys(userSocketMap));
+//   });
+// });
+
+const userSocketMap = new Map();
 
 io.on("connection", (socket) => {
-  console.log("A user connected", socket.user.fullName);
+  console.log(
+    "A user connected -",
+    socket.user.fullName,
+    " with socket ID:",
+    socket.id,
+  );
 
   const userId = socket.userId;
-  userSocketMap[userId] = socket.id;
+  const sockets = userSocketMap.get(userId) ?? new Set();
+  sockets.add(socket.id);
+  userSocketMap.set(userId, sockets);
 
-  // io.emit() is used to send events to all connected clients
-  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  io.emit("getOnlineUsers", Array.from(userSocketMap.keys()));
 
-  // with socket.on we listen for events from clients
   socket.on("disconnect", () => {
     console.log("A user disconnected", socket.user.fullName);
-    delete userSocketMap[userId];
-    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    const sockets = userSocketMap.get(userId);
+    if (sockets) {
+      sockets.delete(socket.id);
+      if (sockets.size === 0) {
+        userSocketMap.delete(userId);
+      }
+    }
+    io.emit("getOnlineUsers", Array.from(userSocketMap.keys()));
   });
 });
 
